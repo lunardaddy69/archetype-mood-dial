2340b6135852292a5ce78867676527b84ff44ca5
lunardaddy69 <jacobgib@gmail.com>
2025-05-25 14:34:34 -0600

    Create install.js

---
+++ pages/install.js
@@ -0,0 +1,7 @@
+export default function Install() {
+  return (
+    <div style={{ color: 'white', backgroundColor: 'black', height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+      <h1>Plugin Installed Successfully</h1>
+    </div>
+  );
+}
