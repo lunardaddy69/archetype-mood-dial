6b1117a94364793aaa6f81df9c8886aa37406664
lunardaddy69 <jacobgib@gmail.com>
2025-05-25 14:21:24 -0600

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div style={{ color: 'white', backgroundColor: 'black', height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+      <h1>Archetype Mood Dial is Live</h1>
+    </div>
+  );
+}
