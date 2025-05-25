e266d6d09ecc379330643fc128128810060a27b2
lunardaddy69 <jacobgib@gmail.com>
2025-05-25 14:34:53 -0600

    Create manage.js

---
+++ pages/manage.js
@@ -0,0 +1,7 @@
+export default function Manage() {
+  return (
+    <div style={{ color: 'white', backgroundColor: 'black', height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+      <h1>Manage Your Plugin Settings</h1>
+    </div>
+  );
+}
